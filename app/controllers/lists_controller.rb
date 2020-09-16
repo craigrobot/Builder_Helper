617ed7e777344af3dc18747da8d7a1771e3abb8f
Craig Horan <craighoran8@gmail.com>
2020-09-15 21:23:14 -0400

    completed index method for list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,19 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.user = current_user
+    @list.save
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:project_name)
+  end
 end
