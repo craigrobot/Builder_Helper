cbdd0ce166c3101500595362ae8bd95199015b5e
Vincent Leblanc <vincent.leblanc1990@gmail.com>
2020-09-15 20:39:28 -0400

    controller done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,22 @@
 class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_param)
+    @list.user = current_user
+
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_param
+    params.require(:list).permit(:project_name)
+  end
 end
