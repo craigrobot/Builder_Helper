bf606b668f20c4aaf0d08c3d009ceaebef3ac9b5
Vincent Leblanc <vincent.leblanc1990@gmail.com>
2020-09-17 20:50:58 -0400

    fixed the item_store model availability column

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+
 Store.destroy_all
 Item.destroy_all
+
+puts 'Database clean!'
+
+puts 'Creating data...'
+
 store = Store.create!(name: 'Home Depot', phone_number: '123-333-3456', address: '123 fake street')
 items = Item.create!([{ name: 'Hammer', description: '18 oz, Titanium'}, { name: 'Chisel', description: '4 oz, Steel'}, { name: 'Hammer', description: '20 oz, Steel' }])
 
 ItemStore.create!(item: items.first, store: store, availability: true, price: 2000)
 ItemStore.create!(item: items.second, store: store, availability: true, price: 1800)
 ItemStore.create!(item: items.third, store: store, availability: true, price: 1500)
+
+puts 'Data created!'
