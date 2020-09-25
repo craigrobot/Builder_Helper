6771010011797ae0c6dea97a79d6767531dfafa1
Vincent Leblanc <vincent.leblanc1990@gmail.com>
2020-09-24 21:18:24 -0400

    installed gem geocoder and config the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
   has_many :lists
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
