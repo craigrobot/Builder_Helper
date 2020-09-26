31a80bb1be964e00f81e82c231e6cc7d4fcbf3a7
Craig Horan <craighoran8@gmail.com>
2020-09-26 13:15:20 -0400

    completed pop-up window with overlay and no image button

--- app/javascript/packs/newProjectPopupScript.js
+++ app/javascript/packs/newProjectPopupScript.js
@@ -4,8 +4,9 @@ export const newProjectPopupScript = () => {
 const modalTrigger = document.querySelector("#plus-icon");
 // create if/else statement that checks if modalTrigger variable has content inside of it
   if ( modalTrigger ) {
+    console.log( modalTrigger, "modalTrigger")
   // target popup modal in the DOM and store it in a variable called Modal
-    const modal = document.querySelector('.container-create-project-popup');
+    const modal = document.querySelector('.popup-overlay');
     // (inside if else statement)add event listener to the modalTrigger on user click
     modalTrigger.addEventListener("click", (event) => {
       console.log(modal.classList)
