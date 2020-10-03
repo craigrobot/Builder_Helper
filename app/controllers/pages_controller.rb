e8de74f9bc50b617ba84d13ff441dd656a17df01
Vincent Leblanc <vincent.leblanc1990@gmail.com>
2020-10-03 14:12:00 -0400

    links are done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :check_signed_in
 
   def home
+  end
+
+  private
+
+  def check_signed_in
+    redirect_to lists_path if signed_in?
   end
 
 end
