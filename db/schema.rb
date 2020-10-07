c8eed8cca807ac0fe7ae3abf503a51fbbf757948
Nikolaus Libby <niktimekeeper5@gmail.com>
2020-10-06 20:20:42 -0500

    pushing the edits to home and project page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_03_203732) do
+ActiveRecord::Schema.define(version: 2020_10_06_232337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_10_03_203732) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "project_item_count"
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
